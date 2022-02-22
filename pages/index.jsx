b225389c1efab08e5bba8f2b5d42c0f12af5f73d
Shakhzodbek <b1901275@mdis.uz>
2022-02-22 10:22:21 +0500

    test

---
+++ pages/index.jsx
@@ -0,0 +1,22 @@
+
+import Header from '../src/Components/Header'
+import About from '../src/Components/Home'
+import Gallery from '../src/Components/Gallery'
+import Pricing from '../src/Components/Pricing'
+import Benifits from '../src/Components/Benifits'
+import Contact from '../src/Components/Contact'
+
+export default function Home() {
+  return (
+    <>
+     <section className="all">
+     <Header />
+     <About />
+     <Gallery />
+     <Pricing />
+     <Benifits />
+     <Contact />
+     </section>
+    </>
+  )
+}   
